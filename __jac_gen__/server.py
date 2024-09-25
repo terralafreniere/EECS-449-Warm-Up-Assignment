d1e0d422796c272b9fd580a2839cbb3e434be00a
Terra Lafreniere <terralaf@umich.edu>
2024-09-25 11:11:01 -0400

    Initial commit, built streamlit interface

---
+++ __jac_gen__/server.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from jaclang.plugin.feature import JacFeature as _Jac
+from jaclang.plugin.builtin import *
+from dataclasses import dataclass as __jac_dataclass__
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact(_Jac.Walker):
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, world!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact_with_body(_Jac.Walker):
+    name: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, ' + self.name + '!'})
